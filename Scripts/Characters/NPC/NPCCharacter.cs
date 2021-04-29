35fb95c399e5e2726683a05e0e74c2774952c648
Andrew McGuiness <redtoorange@gmail.com>
2021-04-29 16:10:16 -0400

    Added NPCs

---
+++ Scripts/Characters/NPC/NPCCharacter.cs
@@ -0,0 +1,27 @@
+﻿using Godot;
+
+namespace GameboyRoguelike.Scripts.Characters.NPC
+{
+    public class NPCCharacter : GameCharacter
+    {
+        [Export] private Texture characterSprite;
+        [Export] private NodePath spritePath;
+        public override void _Ready()
+        {
+            base._Ready();
+
+            if (characterSprite == null)
+            {
+                GD.PrintErr("Null characterSprite");
+            }
+            
+            Sprite s = GetNode<Sprite>(spritePath);
+            if (s == null)
+            {
+                GD.PrintErr("Null Sprite Node");
+            }
+            s.Texture = characterSprite;
+            Update();
+        }
+    }
+}
